8ef061322b351b8a4e1b5e2d07b058c4313a52d8
ahom <antoine.hom@gmail.com>
2016-01-26 22:31:36 +0100

    tracis CI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "binr",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Antoine Hom",
     author_email = "antoine.hom@gmail.com",
     description = ("Utilities to read binary files."),
